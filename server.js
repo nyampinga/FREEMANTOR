ecdaff77311abb49c62c3d6007c8444265093832
nyampinga <joselynenya@gmail.com>
2021-07-22 09:58:50 +0200

    this is my database creation

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config({path:'./.env'});
 
 const app = express();
-
+const databaseUrl=process.env.DATABASE;
+mongoose.connect(databaseUrl,{useNewUrlParser:true,useCreateIndex:true,useUnifiedTopology:true,useFindAndModify:false}).then(()=>console.log("Databse connected succesfully")); 
 //port:4040
 app.listen(4040, ()=>{
+    console.log(databaseUrl);
     console.log('server is running on port 4040');
 })
 
